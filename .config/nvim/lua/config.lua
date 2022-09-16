f341ff75a57f0fd64d0da54c47e6cee1e7476e11
Tim Dithmer <tim.dithmer@suresecure.de>
2022-09-16 15:59:06 +0200

    add ctrl-shift-p as command center hjotkey

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -17,6 +17,10 @@ local command_center = require("command_center")
 local noremap = {noremap = true}
 local silent_noremap = {noremap = true, silent = true}
 
+-- open command_center on F1
+vim.api.nvim_set_keymap("n", "<c-P>", ":Telescope command_center<CR>", silent_noremap)
+
+-- command_center keymaps
 command_center.add({
   {
     desc = "Git add current file",
