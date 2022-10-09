a2e4cc1f16a0306f21ece7e548c11c0732c50c9c
Tim Dithmer <dith.tim@gmail.com>
2022-10-09 21:35:33 +0200

    Add snippets and tsserver

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -57,6 +57,7 @@ return require('packer').startup(function(use)
 
     use 'L3MON4D3/LuaSnip'
     use 'saadparwaiz1/cmp_luasnip'
+    use 'honza/vim-snippets'
 
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
