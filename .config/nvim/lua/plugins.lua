befce287a08ec313a918e7c384f4183bff6f487a
Tim Dithmer <tim.dithmer@suresecure.de>
2022-10-12 10:10:29 +0200

    Better startup time with lazy loading snippets

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -51,7 +51,7 @@ return require('packer').startup(function(use)
     use {
         'L3MON4D3/LuaSnip',
         config = function()
-            require("luasnip.loaders.from_snipmate").load()
+            require("luasnip.loaders.from_snipmate").lazy_load()
         end
     }
     use 'saadparwaiz1/cmp_luasnip'
