358f60a35b694c62e9aadc83f354307e7ec35ff1
Tim Dithmer <dith.tim@gmail.com>
2022-11-24 17:33:39 +0100

    Add leader dt to start debugging of go tests but only in go files

--- .config/nvim/after/plugin/dap-go.lua
+++ .config/nvim/after/plugin/dap-go.lua
@@ -1,1 +1,13 @@
 require("dap-go").setup({})
+
+-- map leader dt to debug test
+-- but only in go files
+vim.api.nvim_exec(
+	[[
+augroup dap-go
+  autocmd!
+  autocmd FileType go nmap <silent> <leader>dt <cmd>lua require("dap-go").debug_test()<CR>
+augroup END
+]],
+	false
+)
