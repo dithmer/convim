2e2465f35f3ce2a95b247337638924b15aed9bc9
Tim Dithmer <dith.tim@gmail.com>
2022-12-02 05:54:40 +0100

    Remove auto hover for go files

--- .config/nvim/after/plugin/murmur.lua
+++ .config/nvim/after/plugin/murmur.lua
@@ -5,14 +5,3 @@ local murmur = require("murmur")
 murmur.setup({
 	cursor_rgb = "green",
 })
-
-vim.api.nvim_create_autocmd("CursorHold", {
-	group = MURMUR_AUGROUP,
-	pattern = "*.go",
-	callback = function()
-		-- skip when a float-win already exists.
-		if vim.w.cursor_word ~= "" then
-			vim.lsp.buf.hover()
-		end
-	end,
-})
