c94dee4bfadbdbd03cd73cbd183a7959cc133892
Tim Dithmer <dith.tim@gmail.com>
2022-12-20 21:33:49 +0100

    Add mason atuomation

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,5 +1,7 @@
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+    automatic_installation = true,
+})
 
 -- lsp setup
 --  local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
